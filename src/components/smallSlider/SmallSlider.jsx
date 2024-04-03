0e77a824bd110768774b6795739eef47c847e586
Rohit Singh <153371016+rohitnbt@users.noreply.github.com>
2024-04-03 10:42:01 +0000

    verify fixed

--- src/components/smallSlider/SmallSlider.jsx
+++ src/components/smallSlider/SmallSlider.jsx
@@ -4,11 +4,11 @@ import "./style.scss";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import useFetch from "../../hooks/useFetch";
-import { Verify } from "../verify/verify";
+import { Verify } from "../verify/Verify";
 import { ThreeDotIcon } from "../Icons/ThreeDotIcon";
 
 export const SmallSlider = ({ title }) => {
-    const { data, loading } = useFetch("posts");
+  const { data, loading } = useFetch("posts");
   const settings = {
     className: "center",
     infinite: true,
@@ -21,32 +21,30 @@ export const SmallSlider = ({ title }) => {
     <div className="small-slider">
       <h3>{title}</h3>
 
-        {
-            !loading ? (
-                <Slider {...settings}>
-                    {
-                data?.map((item)=> (
-                    <figure key={item.id} className="slide">
-                        <img src={item.images || item.avatar} alt="" />
-                        <ThreeDotIcon />
-                        <figcaption><span>{item.name}</span> <Verify accountType={item.accountType} /></figcaption>
-                        <div className="img-overlay"></div>
-                    </figure>
-                ))
-}
-                </Slider>
-            ) : ( 
-                    <div className="slide-skeleton">
-                        <div className="slide skeleton"></div>
-                        <div className="slide skeleton"></div>
-                        <div className="slide skeleton"></div>
-                        <div className="slide skeleton"></div>
-                        <div className="slide skeleton"></div>
-                        <div className="slide skeleton"></div>
-                    </div>
-            )
-        }
-      
+      {!loading ? (
+        <Slider {...settings}>
+          {data?.map((item) => (
+            <figure key={item.id} className="slide">
+              <img src={item.images || item.avatar} alt="" />
+              <ThreeDotIcon />
+              <figcaption>
+                <span>{item.name}</span>{" "}
+                <Verify accountType={item.accountType} />
+              </figcaption>
+              <div className="img-overlay"></div>
+            </figure>
+          ))}
+        </Slider>
+      ) : (
+        <div className="slide-skeleton">
+          <div className="slide skeleton"></div>
+          <div className="slide skeleton"></div>
+          <div className="slide skeleton"></div>
+          <div className="slide skeleton"></div>
+          <div className="slide skeleton"></div>
+          <div className="slide skeleton"></div>
+        </div>
+      )}
     </div>
   );
 };
